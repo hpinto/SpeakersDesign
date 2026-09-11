9e4cb71c9b7a9f8b9cc8d11c432293145317b4b0
Héctor A. Pinto F. <hector.pinto@gmail.com>
2026-09-11 13:59:19 -0300

    otro

--- scripts/measures2panels.py
+++ scripts/measures2panels.py
@@ -236,7 +236,7 @@ def calcular_cortes_caja(archivo_txt):
     pdf.add_page()
     
     pdf.set_font("Arial", 'B', 16)
-    pdf.cell(0, 10, f"Planos Acústicos EBS y Deflector Asimétrico - {nombre_parlante}", ln=True, align='C')
+    pdf.cell(0, 10, f"Planos Acústicos EBS y Deflector Asimétrico", ln=True, align='C')
     pdf.cell(0, 10, f"{nombre_parlante}", ln=True, align='C')
     pdf.ln(5)
     
